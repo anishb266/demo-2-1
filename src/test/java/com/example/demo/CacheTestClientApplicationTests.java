b51facf35882ad5061291aa70d0e99db7ea2fc4b
Anish Bhatta <anish@Anishs-MacBook-Pro.local>
2023-10-10 13:10:11 +0530

    messgage

---
+++ src/test/java/com/example/demo/CacheTestClientApplicationTests.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Mockito.times;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Demo.class})
+class CacheTestClientApplicationTests {
+
+	public CacheTestClientApplicationTests() {
+
+	}
+
+	@Test
+	public void test1() {
+		//Mock static method
+		PowerMockito.mockStatic(Demo.class);
+
+		//Set expectation
+		Mockito.when(Demo.message()).thenReturn("Anything!");
+
+		//invoke the method
+		String message = Demo.message();
+
+		//Assert the stub response
+		Assert.assertEquals(message, "Anything!");
+
+		//Verify static method invocation
+		PowerMockito.verifyStatic(Demo.class, times(1));
+		Demo.message();
+	}
+
+}
+
+class Demo {
+
+	public static String message() {
+		return "Hi!";
+	}
+
+}
